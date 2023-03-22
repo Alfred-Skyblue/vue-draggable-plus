2b22ff89a443bdc59f114ba206d07654250cd019
丶远方 <yangpanteng@gmail.com>
2023-03-22 20:57:35 +0800

    build: package

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dts from 'vite-plugin-dts'
-import packageJson from './package.json'
 import UnoCSS from 'unocss/vite'
 export default defineConfig({
   plugins: [vue(), dts(), UnoCSS()],
@@ -19,7 +18,7 @@ export default defineConfig({
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
       // @ts-ignore
-      external: ['vue', ...Object.keys(packageJson.peerDependencies || {})],
+      external: ['vue'],
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
