a9001bfa1cd5054440a74b5ba76ab9e4e33c96d0
Alfred-Skyblue <yangpanteng@gmail.com>
2023-04-02 11:35:31 +0000

    Deploying to gh-pages from @ Alfred-Skyblue/vue-draggable-plus@fc5234328836c08966670d0bee2e2a3178465c4c 🚀

---
+++ assets/chunks/directive.1b2cadb3.js
@@ -0,0 +1,1 @@
+import{u as m}from"./component.1c3f55dc.js";import{q as n}from"../app.4bbadaf4.js";const a={mounted:"mounted",unmounted:"unmounted"},o=new WeakMap,c={[a.mounted](t,e){const s=n(e.value)?[e.value]:e.value,u=m(t,...s);o.set(t,u.destroy)},[a.unmounted](t){var e;(e=o.get(t))==null||e(),o.delete(t)}};export{c as v};
