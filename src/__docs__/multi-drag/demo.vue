184c8619368601ed4e817039f2efe5bcc773a247
Matheus Dias <matheus.foscarinid@gmail.com>
2023-06-13 16:58:52 -0300

    chore: creating the first version of the multi drag documentation using the basic example

---
+++ src/__docs__/multi-drag/demo.vue
@@ -0,0 +1,74 @@
+<template>
+  <button @click="start">start</button>
+  <button @click="pause">pause</button>
+  <button @click="disabled = true">disabled</button>
+  <div class="flex">
+    <VueDraggable
+      ref="el"
+      v-model="list"
+      :disabled="disabled"
+      animation="150"
+      ghostClass="ghost"
+      class="flex flex-col gap-2 p-4 w-300px h-300px m-auto bg-gray-500/5 rounded"
+      @start="onStart"
+      @update="onUpdate"
+    >
+      <div
+        v-for="item in list"
+        :key="item.id"
+        class="cursor-move h-30 bg-gray-500/5 rounded p-3 cursor-move"
+      >
+        {{ item.name }}
+      </div>
+    </VueDraggable>
+    <preview-list :list="list" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { type UseDraggableReturn, VueDraggable } from 'vue-draggable-plus'
+const list = ref([
+  {
+    name: 'Joao',
+    id: 1
+  },
+  {
+    name: 'Jean',
+    id: 2
+  },
+  {
+    name: 'Johanna',
+    id: 3
+  },
+  {
+    name: 'Juan',
+    id: 4
+  }
+])
+
+const el = ref<UseDraggableReturn>()
+const disabled = ref(false)
+function pause() {
+  el.value?.pause()
+}
+
+function start() {
+  el.value?.start()
+}
+
+const onStart = () => {
+  console.log('start')
+}
+
+const onUpdate = () => {
+  console.log('update')
+}
+</script>
+
+<style scoped>
+.ghost {
+  opacity: 0.5;
+  background: #c8ebfb;
+}
+</style>
---
+++ src/__docs__/multi-drag/index.en-US.md
@@ -0,0 +1,30 @@
+---
+map:
+ path: /demo/multi-drag
+---
+
+# Multi Drag Usage
+
+Single list drag and drop sorting, two way data binding.
+
+## Component Usage
+
+<demo src="./demo.vue"
+title="Component usage"
+desc="Use components to complete drag and drop sorting">
+</demo>
+
+
+## Hooks Usage
+<demo src="./hooks.vue"
+title="hooks usage"
+desc="Use hooks to complete drag and drop sorting">
+</demo>
+
+
+## Directive Usage
+
+<demo src="./directive.vue"
+title="directive use"
+desc="使用指令完成拖拽排序">
+</demo>
